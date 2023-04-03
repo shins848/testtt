7eb7e4758f5fb36f94dc5edb7c2c2b2336c475cc
shins848 <113445272+shins848@users.noreply.github.com>
2023-04-03 00:23:23 -0400

    Add files via upload

---
+++ info.js
@@ -0,0 +1,20 @@
+fetch("items.json")
+.then(function(response){
+   return response.json();
+})
+.then(function(items){
+   let placeholder = document.querySelector("#data-output");
+   let out = "";
+   for(let item of items){
+      out += `
+         <tr>
+            <td>${item.Title}</td>
+            <td>${item.Artist}</td>
+            <td> ${item.Date}</td>
+            <td><img src='${item.Cover}'> </td>
+         </tr>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
